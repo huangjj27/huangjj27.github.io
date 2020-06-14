8f7dcd6a4e3393d3020bb324595983fa37bca547
huangjj27 <huangjj.27@qq.com>
2020-06-14 13:00:06 +0800

    更新代码

--- code/ffi_call_cpp/src/main.rs
+++ code/ffi_call_cpp/src/main.rs
@@ -1,12 +1,12 @@
 #[link(name = "sorting", kind = "static")]
 extern "C" {
-    fn interop_sort(arr: &[i32], n: u32);
+    fn interop_sort(arr: *mut i32, n: u32);
 }
 
 pub fn sort_from_cpp(arr: &mut [i32]) {
     unsafe {
-        // 通过传入 数组的长度来保证不会出现越界访问，从而保证函数内存安全
-        interop_sort(arr, arr.len() as u32);
+        // 传入必然有效的数组引用，并通过传入数组的长度来保证不会出现越界访问，从而保证函数内存安全
+        interop_sort(arr as *mut [i32] as *mut i32, arr.len() as u32);
     }
 }
 
